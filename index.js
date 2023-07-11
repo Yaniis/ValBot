4e197805ffb18fa6e100db08f4cb081cbb50ea12
Yaniis <yaniis.guergour@gmail.com>
2023-07-11 04:20:05 +0200

    Auto create Role - FIX

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const axios = require('axios');
-const { GatewayIntentBits  } = require('discord.js');
+const { GatewayIntentBits } = require('discord.js');
 const config = require('./config.json');
 
 const Client = new Discord.Client({ partials: ["CHANNEL"], intents: [
@@ -12,14 +12,53 @@ const Client = new Discord.Client({ partials: ["CHANNEL"], intents: [
     ]});
 
 
-Client.on('ready', () => {
+Client.on('ready', (serverguild) => {
     console.log('Bot connecté');
 });
 
-const roles = {
-    'Platinum 1' : '1127764285473427456',
-    'Immortal 3' : '1127764391069233172'
-};
+
+const defaultRole = [
+     { name: 'Iron 1', color: '#8d8d8d' },
+     { name: 'Iron 2', color: '#8d8d8d' },
+     { name: 'Iron 3', color: '#8d8d8d' },
+     { name: 'Silver 1', color: '#646363' },
+     { name: 'Silver 2', color: '#646363' },
+     { name: 'Silver 3', color: '#646363' },
+     { name: 'Gold 1', color: '#bea614' },
+     { name: 'Gold 2', color: '#bea614' },
+     { name: 'Gold 3', color: '#bea614' },
+     { name: 'Platinum 1', color: '#2e5b97' },
+     { name: 'Platinum 2', color: '#2e5b97' },
+     { name: 'Platinum 3', color: '#2e5b97' },
+     { name: 'Diamond 1', color: '#833a8b' },
+     { name: 'Diamond 2', color: '#833a8b' },
+     { name: 'Diamond 3', color: '#833a8b' },
+     { name: 'Ascendant 1', color: '#33803b' },
+     { name: 'Ascendant 2', color: '#33803b' },
+     { name: 'Ascendant 3', color: '#33803b' },
+     { name: 'Immortal 1', color: '#a01f1f' },
+     { name: 'Immortal 2', color: '#a01f1f' },
+     { name: 'Immortal 3', color: '#a01f1f' },
+     { name: 'Radiant', color: '#ac7303' },
+];
+
+
+Client.on('guildCreate', (server) => {
+
+    defaultRole.forEach(roleData => {
+
+        server.roles.create({
+                name: roleData.name,
+                color: roleData.color,
+                mentionable: true,
+                managed: false,
+        })
+            .then( (r) => {
+                console.log(`Le rôle ${r.name} a été créé dans le serveur ${server.name}.`);
+            })
+            .catch(console.error);
+    });
+});
 
 Client.on('messageCreate', async (message) => {
 
@@ -50,7 +89,8 @@ Client.on('messageCreate', async (message) => {
             let text = response.data;
             rank = text.split(" - ")[0];
 
-            role = message.guild.roles.cache.get(roles[rank]);
+            role = message.guild.roles.cache.find(role => role.name === rank);
+            console.log(rank);
 
             const user = message.author;
             const member = message.guild.members.cache.get(user.id);
@@ -64,11 +104,11 @@ Client.on('messageCreate', async (message) => {
                 member.roles.add(role)
                     .then(() => {
 
-                        let userRole = member.roles.cache.map(role => role.id);
+                        let userRole = member.roles.cache.map(r => r);
 
-                        Object.values(roles).forEach(function (value) {
+                        Object.values(userRole).forEach(function (value) {
 
-                            if (userRole.includes(value) && value !== role.id){ // Vérifie si l'utilisateur ne possède pas déjà un des rôles rank
+                            if (defaultRole.some(obj => obj.name === value.name)){ // Vérifie si l'utilisateur ne possède pas déjà un des rôles rank
                                 member.roles.remove(value);
                                 return; // Arrête la boucle
                             }
